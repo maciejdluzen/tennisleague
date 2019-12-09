3802977dd814a7db254f76b523f3beff89a98f9e
Maciej Dluzen <maciej.dluzen@yahoo.com>
2019-12-09 14:34:50 +0000

    User and Role Entities

---
+++ src/main/java/pl/maciejdluzen/tennisleague/domain/entities/Role.java
@@ -0,0 +1,22 @@
+package pl.maciejdluzen.tennisleague.domain.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@ToString
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+}
