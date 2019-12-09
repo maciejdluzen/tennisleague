0c5f5c8bee9ee889d01e4ec743b94ff1c4d75f38
Maciej Dluzen <maciej.dluzen@yahoo.com>
2019-12-09 13:53:01 +0000

    Initial commit

---
+++ src/main/java/pl/maciejdluzen/tennisleague/TennisleagueApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejdluzen.tennisleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maciejdluzen/tennisleague/TennisleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejdluzen.tennisleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
