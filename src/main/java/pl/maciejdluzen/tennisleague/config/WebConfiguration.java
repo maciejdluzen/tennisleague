f82f841f3edb13455d8500af51650c824fc77544
Maciej Dluzen <maciej.dluzen@yahoo.com>
2020-01-14 12:23:16 +0000

    WebConfiguration

---
+++ src/main/java/pl/maciejdluzen/tennisleague/config/WebConfiguration.java
@@ -0,0 +1,38 @@
+package pl.maciejdluzen.tennisleague.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public LocaleContextResolver localeResolver() {
+        CookieLocaleResolver sessionLocaleResolver = new CookieLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(new Locale("en"));
+        return sessionLocaleResolver;
+    }
+}
